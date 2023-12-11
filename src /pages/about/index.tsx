c9068cd0002570d5503caff472a3c29ab5cd94ac
jinayj <72746105+jinayj@users.noreply.github.com>
2023-12-11 23:51:59 +0530

    Add files via upload

---
+++ src /pages/about/index.tsx
@@ -0,0 +1,13 @@
+
+import MainLayout from '@Layouts/MainLayout/MainLayout'
+import React from 'react'
+
+const About = () => {
+  return (
+    <MainLayout>
+        About page
+    </MainLayout>
+  )
+}
+
+export default About
